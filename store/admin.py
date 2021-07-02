c171580bdeb3943cc84b381dd95f2e93bd77c139
sai-goutham <saigowthambhuma@gmail.com>
2021-07-02 14:53:45 +0530

    Store Functionality

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Variation
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     list_display=('product_name','price','stock','is_available','category','created_date','modified_date','slug')
     prepopulated_fields={'slug':('product_name',)}
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display=('product','variation_category','variation_value','is_active')
+    list_editable=('is_active',)
+    list_filter=('product','variation_category','variation_value')
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Variation,VariationAdmin)
